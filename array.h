db5bec17a06c24a0710c9c576c6138846645aff6
hidirovaka21 <hidirovakamila21@gmail.com>
2026-05-12 21:25:17 +0300

    Update array.h

--- array.h
+++ array.h
@@ -6,52 +6,213 @@ template <typename T>
 class Array {
 public:
     // ======================== Constructors ========================
-
-    Array();                                // empty array, size 0
-    explicit Array(size_t size);            // default-constructed elements
-    Array(size_t size, const T& value);     // all elements = value
+    Array();
+    explicit Array(size_t size);
+    Array(size_t size, const T& value);
 
     // =================== Copy and Move ===========================
-
     Array(const Array& other);
     Array(Array&& other);
     Array& operator=(const Array& other);
     Array& operator=(Array&& other);
 
     // =================== Destructor ==============================
-
     ~Array();
 
     // =================== Element access ==========================
-
-    T&       operator[](size_t index);
+    T& operator[](size_t index);
     const T& operator[](size_t index) const;
 
-    T&       front();
+    T& front();
     const T& front() const;
 
-    T&       back();
+    T& back();
     const T& back() const;
 
-    T*       data();
+    T* data();
     const T* data() const;
 
     // =================== Capacity ================================
-
     size_t size() const;
-    bool   empty() const;
+    bool empty() const;
 
     // =================== Operations ==============================
-
     void fill(const T& value);
     void swap(Array& other);
 
     // =================== Comparison ==============================
-
     bool operator==(const Array& rhs) const;
     bool operator!=(const Array& rhs) const;
 
 private:
-    T*     data_ = nullptr;
+    T* data_ = nullptr;
     size_t size_ = 0;
 };
+
+// ======================== РЕАЛИЗАЦИИ МЕТОДОВ ========================
+
+template<typename T>
+Array<T>::Array() : data_(nullptr), size_(0) {}
+
+template<typename T>
+Array<T>::Array(size_t size) {
+    size_ = size;
+    if (size_ > 0) {
+        data_ = new T[size_]();
+    } else {
+        data_ = nullptr;
+    }
+}
+
+template<typename T>
+Array<T>::Array(size_t size, const T& value) {
+    size_ = size;
+    if (size_ > 0) {
+        data_ = new T[size_];
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = value;
+        }
+    } else {
+        data_ = nullptr;
+    }
+}
+
+template<typename T>
+Array<T>::Array(const Array& other) {
+    size_ = other.size_;
+    if (size_ > 0) {
+        data_ = new T[size_];
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+    } else {
+        data_ = nullptr;
+    }
+}
+
+template<typename T>
+Array<T>::Array(Array&& other) {
+    data_ = other.data_;
+    size_ = other.size_;
+    other.data_ = nullptr;
+    other.size_ = 0;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array& other) {
+    if (this != &other) {
+        delete[] data_;
+        size_ = other.size_;
+        if (size_ > 0) {
+            data_ = new T[size_];
+            for (size_t i = 0; i < size_; i++) {
+                data_[i] = other.data_[i];
+            }
+        } else {
+            data_ = nullptr;
+        }
+    }
+    return *this;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(Array&& other) {
+    if (this != &other) {
+        delete[] data_;
+        data_ = other.data_;
+        size_ = other.size_;
+        other.data_ = nullptr;
+        other.size_ = 0;
+    }
+    return *this;
+}
+
+template<typename T>
+Array<T>::~Array() {
+    delete[] data_;
+}
+
+template<typename T>
+T& Array<T>::operator[](size_t index) {
+    return data_[index];
+}
+
+template<typename T>
+const T& Array<T>::operator[](size_t index) const {
+    return data_[index];
+}
+
+template<typename T>
+T& Array<T>::front() {
+    return data_[0];
+}
+
+template<typename T>
+const T& Array<T>::front() const {
+    return data_[0];
+}
+
+template<typename T>
+T& Array<T>::back() {
+    return data_[size_ - 1];
+}
+
+template<typename T>
+const T& Array<T>::back() const {
+    return data_[size_ - 1];
+}
+
+template<typename T>
+T* Array<T>::data() {
+    return data_;
+}
+
+template<typename T>
+const T* Array<T>::data() const {
+    return data_;
+}
+
+template<typename T>
+size_t Array<T>::size() const {
+    return size_;
+}
+
+template<typename T>
+bool Array<T>::empty() const {
+    return size_ == 0;
+}
+
+template<typename T>
+void Array<T>::fill(const T& value) {
+    for (size_t i = 0; i < size_; i++) {
+        data_[i] = value;
+    }
+}
+
+template<typename T>
+void Array<T>::swap(Array& other) {
+    T* temp_data = data_;
+    size_t temp_size = size_;
+    data_ = other.data_;
+    size_ = other.size_;
+    other.data_ = temp_data;
+    other.size_ = temp_size;
+}
+
+template<typename T>
+bool Array<T>::operator==(const Array& rhs) const {
+    if (size_ != rhs.size_) {
+        return false;
+    }
+    for (size_t i = 0; i < size_; i++) {
+        if (data_[i] != rhs.data_[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+bool Array<T>::operator!=(const Array& rhs) const {
+    return !(*this == rhs);
+}
